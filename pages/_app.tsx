c1ac9e8ff095f614033c5090560b4d3c32bd5c24
Michael Uloth <hello@michaeluloth.com>
2020-11-24 20:06:28 -0500

    convert js files to ts/tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import type { AppProps /*, AppContext */ } from 'next/app'
+
+import '../styles/tailwind.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
