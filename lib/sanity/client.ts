3c8bc8a04da66062420cc361057da0dfba8de245
Michael Uloth <hello@michaeluloth.com>
2020-11-24 23:11:56 -0500

    create sanity client + library helpers

---
+++ lib/sanity/client.ts
@@ -0,0 +1,39 @@
+import {
+  createClient,
+  createImageUrlBuilder,
+  createPortableTextComponent,
+} from 'next-sanity'
+
+const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  useCdn: process.env.NODE_ENV === 'production',
+}
+
+// Set up the client for fetching data in the getProps page functions
+export const sanityClient = createClient(config)
+
+// Set up a preview client with serverless authentication for drafts
+export const previewClient = createClient({
+  ...config,
+  useCdn: false,
+  token: process.env.SANITY_READ_TOKEN,
+})
+
+// Helper function for easily switching between normal client and preview client
+export const getClient = (usePreview: boolean) =>
+  usePreview ? previewClient : sanityClient
+
+/**
+ * Set up a helper function for generating Image URLs with only the asset reference data in your documents.
+ * Read more: https://www.sanity.io/docs/image-url
+ **/
+export const urlFor = source => createImageUrlBuilder(config).image(source)
+
+// Set up Portable Text serialization
+export const PortableText = createPortableTextComponent({
+  ...config,
+  // Serializers passed to @sanity/block-content-to-react
+  // (https://github.com/sanity-io/block-content-to-react)
+  serializers: {},
+})
