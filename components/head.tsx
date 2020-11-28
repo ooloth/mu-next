4e050ebdff9040a1ba461f9ffd426c995d8141ca
Michael Uloth <hello@michaeluloth.com>
2020-11-28 18:08:13 -0500

    Enable experimental optimizeFonts feature

--- components/head.tsx
+++ components/head.tsx
@@ -1,12 +1,15 @@
 import NextHead from 'next/head'
-import GoogleFonts from 'next-google-fonts'
 
 export default function Head() {
   return (
     <>
-      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap" />
       <NextHead>
         <title>Michael Uloth</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
       </NextHead>
     </>
   )
---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  experimental: {
+    optimizeFonts: true,
+  },
+  reactStrictMode: true,
+}
