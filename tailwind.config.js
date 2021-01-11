b18f133ae58ff88399488070f8476392eab29e76
Michael Uloth <hello@michaeluloth.com>
2021-01-10 21:24:13 -0500

    add basic syntax highlighting

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,9 +41,6 @@ module.exports = {
               },
               code: { color: theme('colors.blue.400') },
             },
-            // 'h2,h3,h4': {
-            // 'scroll-margin-top': spacing[32],
-            // },
             code: { color: theme('colors.pink.500') },
             'blockquote p:first-of-type::before': false,
             'blockquote p:last-of-type::after': false,
