b94768b403ab713d7b430498b2b977b6530572d1
Michael Uloth <hello@michaeluloth.com>
2021-01-10 20:33:05 -0500

    add taildwind typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 // Must remain a CommonJS file (not ESM or TS)
 
 const defaultTheme = require('tailwindcss/defaultTheme')
-const allColors = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   darkMode: 'class',
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./components/**/*.{js,ts,tsx}', './pages/**/*.{js,ts,tsx}'],
   theme: {
     colors: {
       ...defaultTheme.colors,
-      gray: allColors.gray,
+      gray: colors.gray,
     },
     extend: {
       fontFamily: {
@@ -30,11 +30,72 @@ module.exports = {
       zIndex: {
         '-1': '-1',
       },
+      typography: theme => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.700'),
+            a: {
+              color: theme('colors.blue.500'),
+              '&:hover': {
+                color: theme('colors.blue.700'),
+              },
+              code: { color: theme('colors.blue.400') },
+            },
+            // 'h2,h3,h4': {
+            // 'scroll-margin-top': spacing[32],
+            // },
+            code: { color: theme('colors.pink.500') },
+            'blockquote p:first-of-type::before': false,
+            'blockquote p:last-of-type::after': false,
+          },
+        },
+        dark: {
+          css: {
+            color: theme('colors.gray.300'),
+            a: {
+              color: theme('colors.blue.400'),
+              '&:hover': {
+                color: theme('colors.blue.600'),
+              },
+              code: { color: theme('colors.blue.400') },
+            },
+            blockquote: {
+              borderLeftColor: theme('colors.gray.700'),
+              color: theme('colors.gray.300'),
+            },
+            'h2,h3,h4': {
+              color: theme('colors.gray.100'),
+            },
+            hr: { borderColor: theme('colors.gray.700') },
+            ol: {
+              li: {
+                '&:before': { color: theme('colors.gray.500') },
+              },
+            },
+            ul: {
+              li: {
+                '&:before': { backgroundColor: theme('colors.gray.500') },
+              },
+            },
+            strong: { color: theme('colors.gray.300') },
+            thead: {
+              color: theme('colors.gray.100'),
+            },
+            tbody: {
+              tr: {
+                borderBottomColor: theme('colors.gray.700'),
+              },
+            },
+          },
+        },
+      }),
     },
   },
   variants: {
     extend: {
       backgroundOpacity: ['dark'],
     },
+    typography: ['dark'],
   },
+  plugins: [require('@tailwindcss/typography')],
 }
