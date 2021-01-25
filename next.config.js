0f47bf6b3df006de1d0039dc3202ea106fc34b9a
Michael Uloth <hello@michaeluloth.com>
2021-01-25 00:38:02 -0500

    add redirects

--- next.config.js
+++ next.config.js
@@ -6,4 +6,53 @@ module.exports = {
     domains: ['media.giphy.com'],
   },
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/codes',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/contact',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/likes',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/opera',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/sings',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/tax-preparation',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/web-design',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/websites',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/writes',
+        destination: '/blog',
+        permanent: true,
+      },
+    ]
+  },
 }
