fdda0590471acfd6ed95e45fcf89d04410311b2f
Michael Uloth <hello@michaeluloth.com>
2021-01-31 18:54:59 -0500

    let emoji component handle pairing pictures with labels

--- components/emoji.tsx
+++ components/emoji.tsx
@@ -1,14 +1,26 @@
+const emojiLabel = {
+  '📖': 'An emoji of an open book.',
+  '✍': 'An emoji of a hand writing with a pen.',
+  '📺': 'An emoji of a televison.',
+  '🎧': 'An emoji of a pair of large black headphones.',
+  '👩‍💻': 'An emoji of a person writing software code on a laptop.',
+  '📤': 'An emoji of an outbox tray.',
+  '☀️': 'An emoji of a sun.',
+  '🌙️': 'An emoji of a crescent moon.',
+}
+
+export type EmojiPicture = keyof typeof emojiLabel
+
 export interface EmojiProps {
-  picture: string
-  label: string
+  picture: EmojiPicture
   className?: string
 }
 
-export default function Emoji({ picture, label, className, ...props }: EmojiProps) {
+export default function Emoji({ picture, className, ...props }: EmojiProps) {
   return (
     <span
       role="img"
-      aria-label={label}
+      aria-label={emojiLabel[picture]}
       className={`flex-none ${className || ''}`}
       {...props}
     >
