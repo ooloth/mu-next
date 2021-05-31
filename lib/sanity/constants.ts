f332aa25e44da94aa0aa4d063f3dee938f1119a0
Michael Uloth <hello@michaeluloth.com>
2021-05-31 19:17:39 -0400

    delete unused sanity files

--- lib/sanity/constants.ts
+++
@@ -1,3 +0,0 @@
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-export const useCdn = process.env.NODE_ENV === 'production'
