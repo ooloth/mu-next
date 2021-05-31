c12edc33bcab61703ae4ed91c595ed980789fa6b
Michael Uloth <hello@michaeluloth.com>
2021-05-31 19:21:45 -0400

    connect to notion books, albums and podcasts databases

---
+++ lib/notion/constants.ts
@@ -0,0 +1,9 @@
+export const blogPostsDbId = '240463488bbc4c598888754f0e15e1e0'
+
+export const milestonesDbId = 'a32cf1b434a2486c9ab8d4b537e3eff7'
+
+export const booksDbId = '7a7fde563f50456b90ddc1d640fd49bd'
+
+export const albumsDbId = 'c4fe93fd41cf458b9d79716b81cee30a'
+
+export const podcastsDbId = '219598ca134543929e4039b5518f30a5'
--- pages/likes.tsx
+++ pages/likes.tsx
@@ -4,11 +4,11 @@ import { NextSeo } from 'next-seo'
 
 import Outer from 'layouts/outer'
 import Header from 'components/header'
-import albumList from '../content/likes/albums'
-import bookList from '../content/likes/books'
-import podcastList from '../content/likes/podcasts'
-import fetchTmdbList, { TmdbItem } from 'lib/tmdb/fetchTmdbList'
+import notion from 'lib/notion/client'
+import { albumsDbId, booksDbId, podcastsDbId } from 'lib/notion/constants'
 import fetchItunesItems, { iTunesItem } from 'lib/itunes/fetchItunesItems'
+import fetchTmdbList, { TmdbItem } from 'lib/tmdb/fetchTmdbList'
+import { tmdbMovieListId, tmdbTvListId } from 'lib/tmdb/constants'
 
 const seo = {
   url: 'https://michaeluloth.com/likes',
@@ -61,50 +61,81 @@ interface LikesCategory {
   info: string
 }
 
-const Category = ({ heading, items, info }: LikesCategory) => (
-  <section className="mt-16">
-    <h2 className="text-5xl font-extrabold">{heading}</h2>
+function Category({ heading, items, info }: LikesCategory) {
+  return (
+    <section className="mt-16">
+      <h2 className="text-5xl font-extrabold">{heading}</h2>
 
-    <ul className="flex relative mt-6 overflow-x-auto overflow-y-hidden hide-scrollbar scrolling-touch">
-      {items.map(item => (
-        <li key={item.id} className="flex-none mr-10 w-48">
-          <Link
-            href={item.link || 'https://youtu.be/dQw4w9WgXcQ'}
-            aria-label={`Visit the ${info} page for "${item.title}" in a new window.`}
-          >
-            <a>
-              <Image
-                src={item.imageUrl}
-                alt="" // decorative, so hide from screen readers
-                width={300}
-                height={heading === 'Albums' || heading === 'Podcasts' ? 300 : 435}
-                className="shadow-lg rounded bg-gray-900"
-              />
+      <ul className="flex relative mt-6 overflow-x-auto overflow-y-hidden hide-scrollbar scrolling-touch">
+        {items.map(item => (
+          <li key={item.id} className="flex-none mr-10 w-48">
+            <Link
+              href={item.link || 'https://youtu.be/dQw4w9WgXcQ'}
+              aria-label={`Visit the ${info} page for "${item.title}" in a new window.`}
+            >
+              <a>
+                <Image
+                  src={item.imageUrl}
+                  alt="" // decorative, so hide from screen readers
+                  width={300}
+                  height={
+                    heading === 'Albums' || heading === 'Podcasts' ? 300 : 435
+                  }
+                  className="shadow-lg rounded bg-gray-900"
+                />
 
-              <p className="mt-2 leading-snug sm:leading-tight text-center font-semibold">
-                {item.title}
-              </p>
-
-              {'artist' in item && item.artist && (
-                <p className="mt-1 text-sm text-center font-semibold">
-                  {item.artist}
+                <p className="mt-2 leading-snug sm:leading-tight text-center font-semibold">
+                  {item.title}
                 </p>
-              )}
 
-              {item.date && (
-                <p className="mt-1 text-sm text-center font-semibold">
-                  ({new Date(item.date).getFullYear()})
-                </p>
-              )}
-            </a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </section>
-)
+                {'artist' in item && item.artist && (
+                  <p className="mt-1 text-sm text-center font-semibold">
+                    {item.artist}
+                  </p>
+                )}
+
+                {item.date && (
+                  <p className="mt-1 text-sm text-center font-semibold">
+                    ({new Date(item.date).getFullYear()})
+                  </p>
+                )}
+              </a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
+
+async function fetchNotionLikesContent(notionDbId: string) {
+  const notionDbRows = await notion.databases.query({
+    database_id: notionDbId,
+  })
+
+  const contentList = notionDbRows.results.reduce((validatedItems, item) => {
+    // @ts-ignore
+    const name = item.properties['Title']?.title[0]?.plain_text
+    // @ts-ignore
+    const id = item.properties['Apple ID']?.number
+    // @ts-ignore
+    const date = item.properties['Date']?.date.start
+
+    if (name && id && date) {
+      return [...validatedItems, { name, id, date }]
+    }
+
+    return validatedItems
+  }, [])
+
+  return contentList
+}
 
 export async function getStaticProps() {
+  const bookList = await fetchNotionLikesContent(booksDbId)
+  const albumList = await fetchNotionLikesContent(albumsDbId)
+  const podcastList = await fetchNotionLikesContent(podcastsDbId)
+
   let tvShows: TmdbItem[],
     movies: TmdbItem[],
     books: iTunesItem[],
@@ -112,8 +143,8 @@ export async function getStaticProps() {
     podcasts: iTunesItem[]
 
   await Promise.all([
-    (tvShows = await fetchTmdbList(process.env.TMDB_TV_LIST_ID, 'tv')),
-    (movies = await fetchTmdbList(process.env.TMDB_MOVIE_LIST_ID, 'movie')),
+    (tvShows = await fetchTmdbList(tmdbTvListId, 'tv')),
+    (movies = await fetchTmdbList(tmdbMovieListId, 'movie')),
     (books = await fetchItunesItems(bookList, 'ebook', 'ebook')),
     (albums = await fetchItunesItems(albumList, 'music', 'album')),
     (podcasts = await fetchItunesItems(podcastList, 'podcast', 'podcast')),
