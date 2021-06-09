7a05e73a2b137e3e8e7da153f5f664c3a2585fef
Michael Uloth <hello@michaeluloth.com>
2021-06-08 23:16:06 -0400

    add topics and bookmarks db ids

--- lib/notion/constants.ts
+++ lib/notion/constants.ts
@@ -1,9 +1,13 @@
+export const albumsDbId = 'c4fe93fd41cf458b9d79716b81cee30a'
+
 export const blogPostsDbId = '240463488bbc4c598888754f0e15e1e0'
 
-export const milestonesDbId = 'a32cf1b434a2486c9ab8d4b537e3eff7'
+export const bookmarksDbId = '647a78406c5c4121b0ab174018d0d9c7'
 
 export const booksDbId = '7a7fde563f50456b90ddc1d640fd49bd'
 
-export const albumsDbId = 'c4fe93fd41cf458b9d79716b81cee30a'
+export const milestonesDbId = 'a32cf1b434a2486c9ab8d4b537e3eff7'
 
 export const podcastsDbId = '219598ca134543929e4039b5518f30a5'
+
+export const topicsDbId = '2e756d53b0f34273bd7316ceb9fe333d'
