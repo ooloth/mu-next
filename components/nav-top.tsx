4301bf4ccf13868ab419fc7716a22aac6da28816
Michael Uloth <hello@michaeluloth.com>
2021-06-13 20:03:00 -0400

    add bookmarks to top nav on non-mobile screens

--- components/nav-top.tsx
+++ components/nav-top.tsx
@@ -1,10 +1,78 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-import Emoji, { EmojiPicture } from './emoji'
+// import Emoji, { EmojiPicture } from './emoji'
 import notCurrentPage from '../utils/not-current-page'
 import classNames from '../utils/class-names'
-import { useColorMode } from './color-mode'
+// import { useColorMode } from './color-mode'
+
+export default function TopNav() {
+  const { pathname } = useRouter()
+
+  return (
+    <nav
+      className="flex justify-center items-center fixed z-50 top-0 bg-gray-900 bg-opacity-60 py-1 w-full"
+      style={{
+        WebkitBackdropFilter: 'saturate(180%) blur(20px)',
+        backdropFilter: 'saturate(180%) blur(20px)',
+      }}
+    >
+      {/*<ToggleDarkMode />*/}
+
+      <ul className="flex">
+        {topNavItems.map(item => (
+          <TopNavItem
+            key={item.text}
+            href={item.href}
+            text={item.text}
+            active={!notCurrentPage(pathname, item.href)}
+          />
+        ))}
+
+        <TopNavItem
+          key="Bookmarks"
+          href="/bookmarks"
+          text="Bookmarks"
+          active={!notCurrentPage(pathname, '/bookmarks')}
+          className="hidden sm:inline-flex"
+        />
+      </ul>
+    </nav>
+  )
+}
+
+function TopNavItem({ href, text, active = false, className }: NavItem) {
+  const baseClasses =
+    'mx-0.5 inline-flex rounded py-2 px-4 sm:px-7 text-sm font-semibold'
+
+  const activeColors = 'bg-blue-600 bg-opacity-20 text-blue-400'
+  const inactiveColors =
+    'text-gray-300 bg-transparent hover:bg-blue-600 hover:bg-opacity-20 hover:text-blue-400'
+
+  const colorClasses = active ? activeColors : inactiveColors
+
+  return (
+    <li className="leading-none">
+      <Link href={href}>
+        <a className={classNames([baseClasses, colorClasses, className])}>{text}</a>
+      </Link>
+    </li>
+  )
+}
+
+export interface NavItem {
+  href: string
+  text: string
+  active?: boolean
+  className?: any
+}
+
+const topNavItems: NavItem[] = [
+  { href: '/', text: 'Home' },
+  { href: '/about', text: 'About' },
+  { href: '/writing', text: 'Writing' },
+  // "Bookmarks" is added separately above with conditional styling
+]
 
 // function ToggleDarkMode() {
 //   const { colorMode, setColorMode } = useColorMode()
@@ -24,64 +92,3 @@ import { useColorMode } from './color-mode'
 //     </button>
 //   )
 // }
-
-export interface NavItem {
-  href: string
-  text: string
-  active?: boolean
-}
-
-const topNavItems: NavItem[] = [
-  { href: '/', text: 'Home' },
-  { href: '/about', text: 'About' },
-  { href: '/blog', text: 'Blog' },
-  { href: '/notes', text: 'Notes' },
-]
-
-function TopNavItem({ href, text, active = false }: NavItem) {
-  const baseClasses =
-    'mx-0.5 inline-flex rounded py-2 px-4 sm:px-7 text-sm font-semibold'
-
-  const activeColors = 'bg-blue-600 bg-opacity-20 text-blue-400'
-  const inactiveColors =
-    'text-gray-300 bg-transparent hover:bg-blue-600 hover:bg-opacity-20 hover:text-blue-400'
-
-  const colorClasses = active ? activeColors : inactiveColors
-
-  return (
-    <li className="leading-none">
-      <Link href={href}>
-        <a className={classNames([baseClasses, colorClasses])}>{text}</a>
-      </Link>
-    </li>
-  )
-}
-
-export default function TopNav() {
-  const { pathname } = useRouter()
-
-  return (
-    <nav
-      className="flex justify-center items-center fixed z-50 top-0 bg-gray-900 bg-opacity-60 py-1 w-full"
-      style={{
-        WebkitBackdropFilter: 'saturate(180%) blur(20px)',
-        backdropFilter: 'saturate(180%) blur(20px)',
-      }}
-    >
-      {/*<ToggleDarkMode />*/}
-
-      <ul className="flex">
-        {topNavItems
-          // .filter(item => notCurrentPage(pathname, item.href))
-          .map(item => (
-            <TopNavItem
-              key={item.text}
-              href={item.href}
-              text={item.text}
-              active={!notCurrentPage(pathname, item.href)}
-            />
-          ))}
-      </ul>
-    </nav>
-  )
-}
