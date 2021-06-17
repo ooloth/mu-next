b391df5bd03b8616ca11dce87f5f24ea8f226387
Michael Uloth <hello@michaeluloth.com>
2021-06-16 22:49:08 -0400

    move font loading to document level

--- layouts/outer.tsx
+++ layouts/outer.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react'
 
-import Head from '../components/head'
 import TopNav from '../components/nav-top'
 import Footer from '../components/footer'
 
@@ -12,7 +11,6 @@ interface OuterProps {
 export default function Outer({ narrow, children }: OuterProps) {
   return (
     <>
-      <Head />
       <TopNav />
       <div
         className={`flex flex-col pt-28 px-6 min-h-screen ${
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,48 +1,64 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+// import Script from 'next/script'
 import React from 'react'
 
 import { googleSearchConsoleSiteVerification } from 'lib/google/constants'
 
-function SetColorModeBeforeFirstRender({ defaultMode }) {
-  // Runs only once (when the first page loads) to avoid a FOUC when the content appears
+// function SetColorModeBeforeFirstRender({ defaultMode }) {
+//   // Runs only once (when the first page loads) to avoid a FOUC when the content appears
 
-  const lightUnlessDarkPrefered = `
-    (function() {
-      const userChoseDark = localStorage.theme === 'dark'
+//   const lightUnlessDarkPrefered = `
+//     (function() {
+//       const userChoseDark = localStorage.theme === 'dark'
 
-      const userPrefersDark =
-        !('theme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches
+//       const userPrefersDark =
+//         !('theme' in localStorage) &&
+//         window.matchMedia('(prefers-color-scheme: dark)').matches
 
-      if (userChoseDark || userPrefersDark) {
-        document.querySelector('html').classList.add('dark')
-      } else {
-        document.querySelector('html').classList.remove('dark')
-      }
-    })()
-  `
+//       if (userChoseDark || userPrefersDark) {
+//         document.querySelector('html').classList.add('dark')
+//       } else {
+//         document.querySelector('html').classList.remove('dark')
+//       }
+//     })()
+//   `
 
-  const darkUnlessLightChosen = `
-    (function() {
-      const userChoseLight = localStorage.theme === 'light'
+//   const darkUnlessLightChosen = `
+//     (function() {
+//       const userChoseLight = localStorage.theme === 'light'
 
-      if (userChoseLight) {
-        document.querySelector('html').classList.remove('dark')
-      }
-    })()
-    `
+//       if (userChoseLight) {
+//         document.querySelector('html').classList.remove('dark')
+//       }
+//     })()
+//     `
 
-  const setInitialColorMode =
-    defaultMode === 'light' ? lightUnlessDarkPrefered : darkUnlessLightChosen
+//   const setInitialColorMode =
+//     defaultMode === 'light' ? lightUnlessDarkPrefered : darkUnlessLightChosen
 
-  return <script dangerouslySetInnerHTML={{ __html: setInitialColorMode }} />
-}
+//   // return <script dangerouslySetInnerHTML={{ __html: setInitialColorMode }} />
+
+//   return (
+//     <Script
+//       dangerouslySetInnerHTML={{ __html: setInitialColorMode }}
+//       strategy="beforeInteractive" // lazyOnload, afterInteractive
+//     />
+//   )
+// }
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="dark">
         <Head>
+          {/* Fonts */}
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;800&display=swap"
+            rel="stylesheet"
+          />
+
+          {/* Favicons */}
           <link rel="shortcut icon" href="/favicons/favicon.ico" />
           <link rel="manifest" href="/favicons/site.webmanifest" />
           <link
@@ -65,12 +81,16 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#ffffff" />
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+
+          {/* RSS */}
           <link
             rel="alternate"
             type="application/rss+xml"
             title="RSS for blog posts"
             href="https://michaeluloth.com/rss.xml"
           />
+
+          {/* Site verification */}
           <meta
             name="google-site-verification"
             content={googleSearchConsoleSiteVerification}
@@ -78,7 +98,7 @@ class MyDocument extends Document {
         </Head>
 
         <body className="overflow-x-hidden bg-white dark:bg-black min-h-screen font-sans text-gray-600 dark:text-gray-300">
-          <SetColorModeBeforeFirstRender defaultMode="dark" />
+          {/* <SetColorModeBeforeFirstRender defaultMode="dark" /> */}
           <Main />
           <NextScript />
         </body>
