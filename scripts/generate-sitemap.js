884f095dd7ef690b3228079d745fbe8e8cbefa25
Michael Uloth <hello@michaeluloth.com>
2021-12-08 09:53:55 -0500

    minor reformatting

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -5,6 +5,7 @@ const prettier = require('prettier')
 
 ;(async () => {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.yml')
+
   const pages = await globby([
     'content/articles/*.mdx',
     'content/notes/*.mdx',
@@ -16,28 +17,30 @@ const prettier = require('prettier')
   ])
 
   const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${pages
-              .map(page => {
-                const path = page
-                  .replace('content/articles', '')
-                  .replace('content/notes', '')
-                  .replace('content/snippets', '')
-                  .replace('content', '')
-                  .replace('.mdx', '')
-                  .replace('pages', '')
-                  .replace('.tsx', '')
-                const route = path === '/index' ? '' : path
-                return `
-                        <url>
-                            <loc>${`https://michaeluloth.com${route}`}</loc>
-                        </url>
-                    `
-              })
-              .join('')}
-        </urlset>
-    `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${pages
+        .map(page => {
+          const path = page
+            .replace('content/articles', '')
+            .replace('content/notes', '')
+            .replace('content/snippets', '')
+            .replace('content', '')
+            .replace('.mdx', '')
+            .replace('pages', '')
+            .replace('.tsx', '')
+
+          const route = path === '/index' ? '' : path
+
+          return `
+            <url>
+              <loc>${`https://michaeluloth.com${route}`}</loc>
+            </url>
+          `
+        })
+        .join('')}
+    </urlset>
+  `
 
   const formatted = prettier.format(sitemap, {
     ...prettierConfig,
