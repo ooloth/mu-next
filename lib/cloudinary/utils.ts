6761113a3328d4cfa5d850efa9619b2688504518
Michael Uloth <hello@michaeluloth.com>
2022-02-26 14:14:25 -0500

    support cloudinary fetching of youtube video thumbnails

--- lib/cloudinary/utils.ts
+++ lib/cloudinary/utils.ts
@@ -6,6 +6,9 @@ function transformCloudinaryImage(url: string, width: number) {
     if (url.includes('fetch/')) {
       return url.replace('fetch/', `fetch/w_${width},f_auto,q_auto,dpr_2.0/`)
     }
+    if (url.includes('youtube/')) {
+      return url.replace('youtube/', `youtube/w_${width},f_auto,q_auto,dpr_2.0/`)
+    }
   }
 
   return url
