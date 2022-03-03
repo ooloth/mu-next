2dd76cb560f96ec39a909a37b0cc263a33b573f9
Michael Uloth <hello@michaeluloth.com>
2022-03-03 17:36:44 -0500

    delete unused next/image config

--- next.config.js
+++ next.config.js
@@ -4,16 +4,6 @@ module.exports = {
   },
   images: {
     loader: 'cloudinary',
-    // path: '',
-    domains: [
-      'is1-ssl.mzstatic.com',
-      'is2-ssl.mzstatic.com',
-      'is3-ssl.mzstatic.com',
-      'is4-ssl.mzstatic.com',
-      'is5-ssl.mzstatic.com',
-      'media.giphy.com',
-      'res.cloudinary.com',
-    ],
   },
   reactStrictMode: true,
   async redirects() {
